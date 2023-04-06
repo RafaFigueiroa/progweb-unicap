deac041cb5e3c7aef4839662cc512cf73e3698c0
Rafael <rafaelmsfigueiroa@gmail.com>
2023-04-06 15:47:22 -0300

    css e estruturação do html

--- lista-compras/script.js
+++ lista-compras/script.js
@@ -1,11 +1,12 @@
 //armazena todos os inputs da pagina
-const inputNomeItem = document.getElementById("inputNomeItem");
-const inputQtd = document.getElementById("inputQtd");
-const inputPreco = document.getElementById("inputPreco");   
+const inputNomeItem = document.getElementById("input-produto");
+const inputQtd = document.getElementById("input-qtd");
+const inputPreco = document.getElementById("input-preco");
+const tipoQtd = document.getElementById("tipo-qtd");
 
 //armazena os botoes e a tabela
-const btAdicionar = document.getElementById("btAdicionar");
-const btLimpar = document.getElementById("btLimpar");
+const btAdicionar = document.getElementById("bt-adicionar");
+const btLimpar = document.getElementById("bt-limpar");
 const tabela = document.getElementById("table-body");
 
 //matriz que armazena os objetos
@@ -25,32 +26,38 @@ const redesenharTabela = (tabela, listaItens) =>{
     //reescreve a tabela por inteiro
     for(let i = 0; i < listaItens.length; i++){
         const nomeItemText = document.createTextNode(listaItens[i].nomeItem);
-        const qtdText = document.createTextNode(listaItens[i].qtd);
-        const precoText = document.createTextNode(listaItens[i].preco);
+        const qtdText = document.createTextNode(listaItens[i].qtd + " " + listaItens[i].tipo);
+        const precoText = document.createTextNode("R$" + listaItens[i].preco);
+        const valorText = document.createTextNode("R$" + listaItens[i].valor);
 
         const tdNomeItem = document.createElement("td");
         const tdQtd = document.createElement("td");
         const tdPreco = document.createElement("td");
+        const tdValor = document.createElement("td");
 
         tdNomeItem.appendChild(nomeItemText);
         tdQtd.appendChild(qtdText);
         tdPreco.appendChild(precoText);
+        tdValor.appendChild(valorText);
 
         const linhaItem = document.createElement("tr");
 
         linhaItem.appendChild(tdNomeItem);
         linhaItem.appendChild(tdQtd);
         linhaItem.appendChild(tdPreco);
+        linhaItem.appendChild(tdValor);
 
         tabela.appendChild(linhaItem);
     }
 };
 
 //molde para criar uma nova linha de
-function DescricaoItem(nomeItem, qtd, preco){
+function DescricaoItem(nomeItem, qtd, tipo, preco){
     this.nomeItem = nomeItem;
     this.qtd = qtd;
+    this.tipo = tipo;
     this.preco = preco;
+    this.valor = qtd * preco;
 }
 
 
@@ -59,6 +66,7 @@ const handleBtAdicionarClick = () =>{
     const nomeItem = inputNomeItem.value;
     const qtd = inputQtd.value;
     const preco = inputPreco.value;
+    const tipo = tipoQtd.value;
     
     //verifica se algum dos inputs está vazio
     if(!nomeItem || !qtd || !preco){
@@ -66,7 +74,7 @@ const handleBtAdicionarClick = () =>{
         return;
     }
 
-    listaItens.push(new DescricaoItem(nomeItem, qtd, preco));
+    listaItens.push(new DescricaoItem(nomeItem, qtd, tipo, preco));
 
     //chama a função para reescrever a tabela
     redesenharTabela(tabela, listaItens);
